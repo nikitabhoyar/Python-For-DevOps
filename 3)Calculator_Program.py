caf39a83b4d30b5ce027a554cf5fc7c7f70af4b0
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-11-08 15:24:15 +0530

    Calculator program is added

---
+++ 3)Calculator_Program.py
@@ -0,0 +1,26 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print(type(num1))
+print(type(num2))
+
+Sum_of_two_num = num1 + num2
+
+print("Sum of two number is: ", Sum_of_two_num)
+
+Sub_of_two_num = num1 - num2 
+
+print("Subtraction of two number is:  ",Sub_of_two_num)
+
+Mul_of_two_num = num1 * num2
+
+print("Multiplication of two number is: ",num1 * num2)
+
+Div_of_two_num = num1/num2
+
+print("Division of two numbers is: ",Div_of_two_num)
+
+Mod_of_two_num = num1 % num2 
+
+print("Modolous of two number is: ", Mod_of_two_num)
+
